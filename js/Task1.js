16a99c44732b808ecce47c9d3e2b41384edd837d
abinaya-renganathan <133967821+abinaya-renganathan@users.noreply.github.com>
2025-08-10 13:07:38 +0530

    added files

---
+++ js/Task1.js
@@ -0,0 +1,67 @@
+//  a = 10;
+//  var s = 25;
+//  let b = 30;
+//  const c = 40;
+//  console.log(a);
+
+// let a = 50;
+// let b = 45.56;
+// let c = "this is a string";
+// let d = null;
+// let s = undefined;
+// console.log(a)
+
+// let a = 50;
+//  let b = 45.56;
+//  let c = "this is a string";
+//  let d = null;
+//  let s = undefined;
+//  console.log(a,b,c,d,s)
+
+// let a = 50;
+//  let b = 45.56;
+//  let c = "the no : ";
+//  let d = null;
+//  let s = undefined;
+//  console.log(c+(a+b));
+
+// let a = 50;
+//  let b = 50;
+//  let c = "this is a string";
+//  let d = null;
+//  let s = undefined;
+//  if(a==b){
+//    console.log(c); 
+//  }
+
+// let person={name:"john",age:50,city:"NY"}
+
+// let(name,age,city)=person;
+// console.log(person);
+
+// let person={name1:"Abinaya",name2:"Renganathan",fullname:"AbinayaRenganathan"}
+// console.log(person.fullname);
+
+// let name1 = "Abinaya"
+// let name2 = "Renganathan"
+// function fn(name1,name2)
+// {
+//     return name1+" "+name2
+// }
+// let person={name1:name1,name2:name2,fullname:fn(name1,name2)}
+// console.log(person.fullname);
+
+let name1 = "Abinaya"
+let name2 = "Renganathan"
+function fn(name1,name2)
+{
+    return name1+" "+name2
+}
+let person={
+    name1:name1,
+    name2:name2,
+    fullname:fn(name1,name2)
+    {
+        return name1+" "+name2
+    }
+}
---
+++ js/Task2.js
@@ -0,0 +1,81 @@
+// let obj = {
+//     name1:name1,
+//     age: age,
+// }
+// console.log();
+
+// let name1="Abinaya"
+// let age1 = 19;
+// class person{
+//     constructor(name1,age)
+//     {
+//         this.name1=name1;
+//         this.age1=age1;
+//     }
+// }
+// let obj1 = new person(name1,age1)
+// let obj2 = new person("Aruna",18);
+// console.log(obj1)
+// console.log(obj2)
+
+// let setA = new Set();
+// setA.add(1);
+// setA.add(2);
+// setA.add(3);
+// setA.add(4);
+// setA.add(5);
+// console.log(setA);
+
+// const setA = new Set([1,2,3]);
+// const setB = new Set([3,4,5,6]);
+// const union = setA.union(setB);
+// console.log(union);
+
+// const setA = new Set([1,2,3]);
+// const setB = new Set([3,4,5,6]);
+// const difference = setA.difference(setB);
+// console.log(difference);
+
+// const fruits = new Map([
+//     ["apples",500],
+//     ["bananas",300],
+//     ["oranges",200]
+// ])
+// console.log(fruits)
+
+// const capitals = new Map();
+// capitals.set("India","New Delhi");
+// capitals.set("France","Paris");
+// capitals.set(123,"Number Key");
+// console.log(capitals.get("India"));
+// console.log(capitals.has("France"));
+// capitals.delete("France");
+// console.log(capitals);
+
+// const fruits = new Map()
+// fruits.set("apples",500);
+// fruits.set("bananas",300);
+// fruits.set("oranges",200);
+// console.log(fruits.get("apples"));
+// console.log(fruits.has("bananas"));
+// fruits.delete("bananas");
+// console.log(fruits);
+
+// const fruits = new Map(
+//     [
+//         ["apples",500],
+//         ["bananas",300],
+//         ["oranges",200]
+//     ]
+// )
+// console.log(fruits.get("apples"));
+// console.log(fruits.has("bananas"));
+// fruits.delete("bananas");
+// console.log(fruits);
+
+// let obj={a1:["b1","b2","b3"],a2:["b1","b2","b3"],a3:["b1","b2","b3"]}
+// console.log(obj);
+
+let obj={a1:["b1","b2","b3"],a2:["b1","b2","b3"],a3:["b1","b2","b3"]}
+console.table(obj);
+
